fbc1e477bb2c431becfb202298811cac807b2b55
Santiago Cobas <aquaticsanti@gmail.com>
2026-05-14 16:23:12 -0300

    Made the top bar placeholder

--- __main__.py
+++ __main__.py
@@ -23,22 +23,58 @@ if pygame.IS_CE != 1: # AKA the current pygame module is NOT pygame-ce
     raise ModuleNotFoundError("Uh oh! This program expects pygame-ce, NOT regular pygame.")
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("StreamBoy \"Emulator\"")
-pygame.font.init()
 
+# Initialize fonts
+pygame.font.init()
+font = pygame.font.SysFont("freesansbold", 30)
+font_smol = pygame.font.SysFont("freesansbold", 20)
+
+# Draw status bar background
+# NOTE: Right now, the color is fixed at blue. 
+# On actual hardware, you will be able to change this.
 pygame.draw.rect(surface=screen, color=(33, 95, 154),
                  rect=(0, 0, width, 40))
 
-bold_font = pygame.font.SysFont("arial.ttf", 40, bold=False)
-time_render = bold_font.render(f"{datetime.datetime.now().strftime("%H:%M")}", False, (255, 255, 255))
+# Load system time
+time_render = font.render(f"{datetime.datetime.now().strftime("%H:%M")}", False, (255, 255, 255))
 textRect1 = time_render.get_rect()
-textRect1.topleft = (6, 6)
-
+textRect1.center = (45, 22.5)
 screen.blit(time_render, textRect1)
 
+# Load battery percentage logo
+# NOTE: On actual hardware, this logo will change acording to battery percentage.
+battery_logo = pygame.image.load_sized_svg("icons\\battery_5_bar.svg", (35, 35)).convert_alpha()
+batteryRect1 = battery_logo.get_rect()
+batteryRect1.center = (305, 20)
+screen.blit(battery_logo, batteryRect1)
+
+# Load battery percentage text
+# NOTE: Right now, this is a placeholder.
+# When this is ported to actual hardware, this will change.
+batteryText_render = font_smol.render("bat%", False, (255, 255, 255))
+batteryTextRect1 = batteryText_render.get_rect()
+batteryTextRect1.center = (265, 22.5)
+screen.blit(batteryText_render, batteryTextRect1)
+
+# Load Wifi logo
+if checkInternet() == True:
+    wifi_logo = pygame.image.load_sized_svg("icons\\wifi.svg", (35, 35)).convert_alpha()
+else:
+    wifi_logo = pygame.image.load_sized_svg("icons\\wifi_off.svg", (35, 35)).convert_alpha()
+wifiRect1 = wifi_logo.get_rect()
+wifiRect1.center = (217.5, 20)
+screen.blit(wifi_logo, wifiRect1)
+
+# Load audio logo
+# NOTE: Right now, it assumes you have no audio plugged in. 
+# When this is ported to actual hardware, this will change.
+audio_logo = pygame.image.load_sized_svg("icons\\close.svg", (45, 45)).convert_alpha()
+audioRect1 = audio_logo.get_rect()
+audioRect1.center = (180, 22.5)
+screen.blit(audio_logo, audioRect1)
+
+# Loop
 pygame.display.flip()
-
-
-# Game loop
 running = True
 while running:
     for event in pygame.event.get():
