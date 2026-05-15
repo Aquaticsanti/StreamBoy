d323173bfa0e44599f5e22875fd4fd44cb59ea2f
Santiago Cobas <aquaticsanti@gmail.com>
2026-05-15 20:54:46 -0300

    Englobed drawing the home menu into its own function

--- __main__.py
+++ __main__.py
@@ -62,52 +62,42 @@ def DrawStatusBar():
     audioRect1 = audio_logo.get_rect()
     audioRect1.center = (180, 22.5)
     screen.blit(audio_logo, audioRect1)
-    primaryColor, secondaryColor = (33, 95, 154), (22, 62, 100)
-    width, height = 320, 240
 
-if pygame.IS_CE != 1: # AKA the current pygame module is NOT pygame-ce
-    raise ModuleNotFoundError("Uh oh! This program expects pygame-ce, NOT regular pygame.")
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("StreamBoy \"Emulator\"")
+def DrawHomeMenu():
+    global screen, primaryColor, secondaryColor, font, font_smol
+    ## Load music button
+    # Load music button background
+    music_bg = pygame.draw.rect(surface=screen, color=primaryColor, rect=(97.5, 80, 150, 50), border_radius=60)
+    # Load music logo
+    music_logo = pygame.image.load_sized_svg("icons\\music_note_2.svg", (50, 50)).convert_alpha()
+    musicRect1 = music_logo.get_rect()
+    musicRect1.center = (107.5, 105)
+    musicCircle2 = pygame.draw.circle(surface=screen, center=musicRect1.center, color=(0, 0, 0), radius=37.5)
+    musicCircle1 = pygame.draw.circle(surface=screen, center=musicRect1.center, color=secondaryColor, radius=30)
+    screen.blit(music_logo, musicRect1)
+    # Load "Music" text
+    musicText_render = font.render("Music", False, (255, 255, 255))
+    musicTextRect1 = musicText_render.get_rect()
+    musicTextRect1.center = (192.5, 107.5)
+    screen.blit(musicText_render, musicTextRect1)
 
-# Initialize fonts
-pygame.font.init()
-font = pygame.font.SysFont("freesansbold", 30)
-font_smol = pygame.font.SysFont("freesansbold", 20)
+    ## Load settings button
+    # Load settings button background
+    settings_bg = pygame.draw.rect(surface=screen, color=secondaryColor, rect=(72.5, 145, 80, 50), border_radius=60)
+    # Load settings logo
+    settings_logo = pygame.image.load_sized_svg("icons\\settings.svg", (47.5, 47.5)).convert_alpha()
+    settingsRect1 = settings_logo.get_rect()
+    settingsRect1.center = (112.5, 170)
+    screen.blit(settings_logo, settingsRect1)
 
-## Load music button
-# Load music button background
-music_bg = pygame.draw.rect(surface=screen, color=primaryColor, rect=(97.5, 80, 150, 50), border_radius=60)
-# Load music logo
-music_logo = pygame.image.load_sized_svg("icons\\music_note_2.svg", (50, 50)).convert_alpha()
-musicRect1 = music_logo.get_rect()
-musicRect1.center = (107.5, 105)
-musicCircle2 = pygame.draw.circle(surface=screen, center=musicRect1.center, color=(0, 0, 0), radius=37.5)
-musicCircle1 = pygame.draw.circle(surface=screen, center=musicRect1.center, color=secondaryColor, radius=30)
-screen.blit(music_logo, musicRect1)
-# Load "Music" text
-musicText_render = font.render("Music", False, (255, 255, 255))
-musicTextRect1 = musicText_render.get_rect()
-musicTextRect1.center = (192.5, 107.5)
-screen.blit(musicText_render, musicTextRect1)
-
-## Load settings button
-# Load settings button background
-settings_bg = pygame.draw.rect(surface=screen, color=secondaryColor, rect=(72.5, 145, 80, 50), border_radius=60)
-# Load settings logo
-settings_logo = pygame.image.load_sized_svg("icons\\settings.svg", (47.5, 47.5)).convert_alpha()
-settingsRect1 = settings_logo.get_rect()
-settingsRect1.center = (112.5, 170)
-screen.blit(settings_logo, settingsRect1)
-
-## Load apps button
-# Load apps button background
-apps_bg = pygame.draw.rect(surface=screen, color=secondaryColor, rect=(167.5, 145, 80, 50), border_radius=60)
-# Load apps logo
-apps_logo = pygame.image.load_sized_svg("icons\\apps.svg", (47.5, 47.5)).convert_alpha()
-appsRect1 = apps_logo.get_rect()
-appsRect1.center = (207.5, 170)
-screen.blit(apps_logo, appsRect1)
+    ## Load apps button
+    # Load apps button background
+    apps_bg = pygame.draw.rect(surface=screen, color=secondaryColor, rect=(167.5, 145, 80, 50), border_radius=60)
+    # Load apps logo
+    apps_logo = pygame.image.load_sized_svg("icons\\apps.svg", (47.5, 47.5)).convert_alpha()
+    appsRect1 = apps_logo.get_rect()
+    appsRect1.center = (207.5, 170)
+    screen.blit(apps_logo, appsRect1)
 
 selected_option = -1
 def godFunc():
